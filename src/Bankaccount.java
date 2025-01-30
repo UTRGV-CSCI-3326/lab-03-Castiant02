c94572b432e64f4bb07761dd61cd622a8ff98181
Castiant02 <antonio.castillo03@utrgv.edu>
2025-01-30 23:14:47 +0000

    done

---
+++ src/Bankaccount.java
@@ -0,0 +1,16 @@
+public class Bankaccount{
+    public static void main(String[] args){
+        double balance=2175.37 ;
+        balance -= 302.50 ;
+        balance += 50.03 ;
+        balance -= balance/2 ;
+        balance +=20.00 ;
+        balance -=1 ;
+        balance = balance*2;
+        balance += 1; 
+        System.out.print("Bobs balance: "+ balance);
+
+    }
+
+    }
+
